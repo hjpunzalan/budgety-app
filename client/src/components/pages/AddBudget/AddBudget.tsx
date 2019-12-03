a5c15fa11d44d5c3ed0e032bdb320162ec4ea090
hjpunzalan <hj.punzalan@hotmail.com>
2019-12-03 12:44:35 +0800

    Started to add budget and transaction components

---
+++ client/src/components/pages/AddBudget/AddBudget.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+interface Props {}
+interface State {}
+
+class AddBudget extends Component<Props, State> {
+	state = {};
+
+	render() {
+		return (
+			<div>
+				<h1>test</h1>
+				<h1>test</h1>
+				<h1>test</h1>
+				<h1>test</h1>
+				<h1>test</h1>
+				<h1>test</h1>
+				<h1>test</h1>
+			</div>
+		);
+	}
+}
+
+export default AddBudget;
