7cd9852010a372d2a2b78e185af5eeb748049224
hjpunzalan <hj.punzalan@hotmail.com>
2019-12-08 22:44:46 +0800

    cleaned budgetController and added startingBalance to all handlers

--- server/controllers/budgetController.ts
+++ server/controllers/budgetController.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction, Router } from "express";
 import { Users } from "./../models/Users";
 import { Budget } from "./../models/Budget";
-import { IUser } from "./../interfaces/User";
 import { checkBody } from "../utils/checkBody";
 import { AppError } from "./../utils/appError";
 import {
@@ -21,32 +20,24 @@ export const budgetRoute = Router();
 @controller("/budget", budgetRoute)
 class budgetController {
 	@post("/new")
-	@use(requireAuth, bodyValidator("name", "categories"))
+	@use(requireAuth, bodyValidator("name", "categories", "startingBalance"))
 	@catchAsync
 	async newBudget(req: Request, res: Response, next: NextFunction) {
 		interface ReqBody {
 			name: string;
 			categories: string[];
+			startingBalance: number;
 		}
-		const { categories, name }: ReqBody = req.body;
+		const { categories, name, startingBalance }: ReqBody = req.body;
 		// Member will have to declare categories beforehand or update budget
 
 		if (req.session) {
 			const budget = await Budget.create({
 				categories,
 				name,
+				startingBalance,
 				user: req.session.userId
 			});
-			const user = await Users.findById(req.session.userId);
-
-			// Add budget in user's document
-			if (user) {
-				user.budgets.push({
-					budgetId: budget._id,
-					name: budget.name
-				});
-				await user.save();
-			} else return next(new AppError("User not found!", 404));
 			// Send only the new budget to the client
 			// Client will update the store state
 			// Starts with empty transaction
@@ -55,7 +46,7 @@ class budgetController {
 		} else return next(new AppError("User no longer logged in", 403));
 	}
 
-	// For updating categories or name of budget
+	// For updating categories,name, startingBalance of budget
 	@patch("/update/:id")
 	@use(requireAuth)
 	@catchAsync
@@ -67,7 +58,7 @@ class budgetController {
 			}
 			const filterBody: ReqBody = checkBody(
 				req.body,
-				["name", "categories"],
+				["name", "categories", "startingBalance"],
 				next
 			);
 
@@ -83,6 +74,17 @@ class budgetController {
 				}
 			);
 			if (!budget) return next(new AppError("No budget found.", 404));
+
+			// Update budget's balance if startingBalance was changed
+			// Update each transaction's balance also
+			if (req.body.startingBalance) {
+				budget.balance = budget.startingBalance;
+				budget.transactions.forEach(t => {
+					t.balance = budget.balance + t.amount;
+					budget.balance += t.amount;
+				});
+				await budget.save();
+			}
 
 			// Return updated budget list to the client
 			// Client updates entire store state
@@ -108,6 +110,22 @@ class budgetController {
 					new AppError("User does not have any budgets connected", 404)
 				);
 			res.status(200).json(budgetsList);
+		} else return next(new AppError("User no longer logged in", 403));
+	}
+
+	// Transaction will be a separate request
+	@get("/:id")
+	@use(requireAuth)
+	@catchAsync
+	async getBudget(req: Request, res: Response, next: NextFunction) {
+		if (req.session) {
+			const budget = await Budget.findOne({
+				_id: req.params.id,
+				user: req.session.userId
+			}).select("-transactions -user -__v");
+			if (!budget)
+				return next(new AppError("No budget belongs to the id", 404));
+			res.status(200).json(budget);
 		} else return next(new AppError("User no longer logged in", 403));
 	}
 
