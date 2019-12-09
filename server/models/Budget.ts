5beb069d7511bd349948308cadece6c72011258c
hjpunzalan <hj.punzalan@hotmail.com>
2019-12-09 22:29:29 +0800

    added starting balance and balance into Budget model

--- server/models/Budget.ts
+++ server/models/Budget.ts
@@ -5,6 +5,8 @@ import { IBudget } from "./../interfaces/Budget";
 const budgetSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	categories: [{ type: String }],
+	startingBalance: { type: Number, required: true },
+	balance: Number,
 	transactions: [
 		{
 			desc: String,
@@ -15,7 +17,8 @@ const budgetSchema = new mongoose.Schema({
 			category: {
 				type: String
 			},
-			amount: Number
+			amount: Number,
+			balance: Number
 		}
 	],
 	user: {
@@ -25,11 +28,13 @@ const budgetSchema = new mongoose.Schema({
 });
 
 // Validates categories
+// Does not work with find update!!!
 budgetSchema.pre<IBudget>("save", function(next) {
 	this.transactions.forEach(t => {
 		const validate = this.categories.includes(t.category);
 		if (!validate) return next(new AppError("Invalid category", 400));
 	});
+	if (this.isNew) this.balance = this.startingBalance;
 	next();
 });
 
