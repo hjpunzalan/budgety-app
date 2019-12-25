237b4b32e78b807753674b407c9bf469463d2d5f
hjpunzalan <hj.punzalan@hotmail.com>
2019-12-25 08:21:17 +0800

    transaction reducer - handle pagination

--- client/src/reducers/transactions.ts
+++ client/src/reducers/transactions.ts
@@ -18,7 +18,40 @@ export const transactionReducer = (
 ) => {
 	switch (action.type) {
 		case ActionTypes.getTransactions:
-			return [...action.payload];
+			// If state already empty
+			if (state.length === 0) return action.payload;
+			else {
+				// Cant put state to a variable as it doesnt like it and will create bugs
+				action.payload.forEach(newGroup => {
+					// Initiate existAlready
+					let existAlready = false;
+					for (let group of state) {
+						// Find if newGroup already exist within transaction groups
+						if (
+							group._id.month === newGroup._id.month &&
+							group._id.year === newGroup._id.year
+						) {
+							// Update group transactions
+							// Be wary of duplicates
+							group.transactions = [
+								...group.transactions,
+								...newGroup.transactions
+							];
+
+							// Set exist to true if found match
+							existAlready = true;
+							break;
+						}
+					}
+					if (!existAlready) {
+						// Conditional to add new group to transaction after loop
+						state.push(newGroup);
+					}
+				});
+
+				/// If newGroup doesnt exist in all of the groups, add the new group to the transaction
+				return state;
+			}
 		case ActionTypes.addTransaction:
 			return action.payload;
 		case ActionTypes.clearTransactions:
