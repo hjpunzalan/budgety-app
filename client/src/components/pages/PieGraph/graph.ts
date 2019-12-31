61f4cdd68ca37cfa4ef89d35ac566cb4245f16ff
hjpunzalan <hj.punzalan@hotmail.com>
2019-12-31 19:16:43 +0800

    pieGraph added tweens to all elements - update, exit ,enter selections

--- client/src/components/pages/PieGraph/graph.ts
+++ client/src/components/pages/PieGraph/graph.ts
@@ -10,6 +10,10 @@ export enum PieGraphType {
 	expense = "expense"
 }
 
+interface PathElement extends SVGPathElement {
+	_current: PieArcDatum<BudgetCategoryData>;
+}
+
 export const graph = (component: PieGraph, size: number, extra: number) => {
 	const dims = { height: size, width: size, radius: size / 2 }; // dimension of the pie chart
 
@@ -40,50 +44,96 @@ export const graph = (component: PieGraph, size: number, extra: number) => {
 			.innerRadius(0);
 
 		// ordinal scale
-		const colour = d3.scaleOrdinal(d3.schemeSet2);
+		const colour = d3.scaleOrdinal(
+			component.props.type === PieGraphType.income
+				? d3.schemeSet2
+				: d3.schemeSet1
+		);
+
+		//////////////////////////////////////// TWEENS //////////////////////////////////////////
+		const arcTweenEnter = (d: PieArcDatum<BudgetCategoryData>) => {
+			let i = d3.interpolate(d.endAngle, d.startAngle);
+
+			return function(t: number) {
+				d.startAngle = i(t);
+				return String(arcPath(d));
+			};
+		};
+
+		const arcTweenExit = (d: PieArcDatum<BudgetCategoryData>) => {
+			let i = d3.interpolate(d.startAngle, d.endAngle);
+
+			return function(t: number) {
+				d.startAngle = i(t);
+				return String(arcPath(d));
+			};
+		};
+		// use function keyword so 'this' will be dynamically scoped
+		// Using updated data
+		function arcTweenUpdate(
+			this: PathElement,
+			d: PieArcDatum<BudgetCategoryData>
+		) {
+			// interpolate between the two objects and not just angles because interpolation may happen in two of the angles or one
+			let i = d3.interpolate(this._current, d);
+
+			// update the current prop with the new updated data for future changes
+			// Initialise a value
+			// Will be replaced as data from enter selection
+			// Update will compare new data from current one
+			this._current = i(1); // or d
+
+			return function(t: number) {
+				return String(arcPath(i(t)));
+			};
+		}
+		////////////////////////////////////////////////////////////////////////////////////////////////
 
 		const graph = d3
 			.select(component.canvas.current)
 			.select("svg")
 			.select("g");
-
+		// Update data
 		const slice = graph.selectAll("g.slice").data(pie(data));
+
+		////////////////////////// Handle current selection//////////////////////////////
+		// Updates labels
 		slice
 			.select("text")
+			.transition()
+			.duration(750)
 			.attr("text-anchor", "middle")
 			.attr("transform", d => `translate(${arcPath.centroid(d)})`)
 			.text(d => budget.categories[d.data._id.category])
 			.attr("class", classes.labels);
 
-		////////////////////////// Handle current selection//////////////////////////////
-		const path = slice.select("path");
+		slice
+			.select<PathElement>("path")
+			.attr("d", arcPath)
+			.attr("fill", d => colour(budget.categories[d.data._id.category])) // update colours
+			.transition()
+			.duration(750)
+			.attrTween("d", arcTweenUpdate);
 
-		path.attr("d", arcPath);
+		/////////////////////////// Handle exit selection/////////////////////////
 
-		path
-			.attr("stroke", "#fff")
-			.attr("stroke-width", 2)
-			.attr("fill", d => colour(budget.categories[d.data._id.category]))
-			.style("cursor", "pointer")
-			.on("mouseover", () => {
-				d3.select(component.tooltip.current)
-					.style("opacity", 1)
-					.style("color", type === PieGraphType.income ? "green" : "red");
-			})
-			.on("mousemove", d => {
-				d3.select(component.tooltip.current)
-					.style("left", d3.event.pageX + 15 + "px")
-					.style("top", d3.event.pageY + "px")
-					.select("#pieGraphTip")
-					.text(
-						`${budget.categories[d.data._id.category]}: ${checkAmount(
-							Math.abs(d.data[type])
-						)}`
-					);
-			})
-			.on("mouseout", d =>
-				d3.select(component.tooltip.current).style("opacity", 0)
-			);
+		slice
+			.exit<PieArcDatum<BudgetCategoryData>>()
+			.select("path")
+			.transition()
+			.duration(750) // 750ms
+			.attrTween("d", arcTweenExit)
+			.remove();
+
+		slice
+			.exit()
+			.select("text")
+			.remove();
+		slice
+			.exit()
+			.transition()
+			.duration(750)
+			.remove();
 		/////////////////////////// Handle enter selection///////////////////////
 		const newSlice = slice
 			.enter()
@@ -91,12 +141,22 @@ export const graph = (component: PieGraph, size: number, extra: number) => {
 			.attr("class", "slice");
 
 		newSlice
-			.append("path")
+			.append<PathElement>("path")
 			.attr("d", arcPath)
 			.attr("stroke", "#fff")
 			.attr("stroke-width", 2)
 			.attr("fill", d => colour(budget.categories[d.data._id.category]))
 			.style("cursor", "pointer")
+			.each(function(d) {
+				this._current = d;
+			})
+			.transition()
+			.duration(750) // 750ms
+			.attrTween("d", arcTweenEnter);
+
+		// Add mouse events
+		newSlice
+			.select("path")
 			.on("mouseover", () => {
 				d3.select(component.tooltip.current)
 					.style("opacity", 1)
@@ -116,15 +176,15 @@ export const graph = (component: PieGraph, size: number, extra: number) => {
 			.on("mouseout", d =>
 				d3.select(component.tooltip.current).style("opacity", 0)
 			);
+
 		newSlice
 			.append("text")
+			.transition()
+			.duration(750)
 			.attr("text-anchor", "middle")
 			.attr("transform", d => `translate(${arcPath.centroid(d)})`)
 			.text(d => budget.categories[d.data._id.category])
 			.attr("class", classes.labels);
-
-		/////////////////////////// Handle exit selection/////////////////////////
-		slice.exit().remove();
 	};
 
 	// Remove graph if there's no amount depending on type
